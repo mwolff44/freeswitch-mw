a72d4ac24c28ac4f6edd8d75b4ff1e8304b6193b
mwolff44 <mathiaswolff@mac.com>
2017-11-23 17:28:30 +0100

    script to install ansible

---
+++ ansible_install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# Ansible install srcipt - Bash Library
+#
+# Copyright (c) 2017 Mathias WOLFF <mathias@celea.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# * Neither the name of Mathias WOLFF nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific prior
+# written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+#
+# Ansible Installer
+# Copyright (c) 2017 M%athias WOLFF.
+#
+# My ref: https://www.blog-des-telecoms.com
+
+#---------
+#VARIABLES
+#---------
+# default distro
+DISTRO=jessie
+
+#---------------------
+#   ENVIRONMENT CHECKS
+#---------------------
+#check for root
+if [ $EUID -ne 0 ]; then
+   /bin/echo "This script must be run as root" 1>&2
+   exit 1
+fi
+echo "Good, you are root."
+
+#check for internet connection
+/usr/bin/wget -q --tries=10 --timeout=5 http://www.google.com -O /tmp/index.google &> /dev/null
+if [ ! -s /tmp/index.google ]; then
+	echo "No Internet connection. Exiting."
+	/bin/rm /tmp/index.google
+	#exit 1
+else
+	echo "Internet connection is working, continuing!"
+	/bin/rm /tmp/index.google
+fi
+
+#check distribution
+DISTRO_CHECK=$(lsb_release -c |sed -e s/Codename://g |sed -r 's/\s+//g')
+
+if [ $DISTRO_CHECK == $DISTRO ]; then 
+    echo "Ansible installation from Ansible packages"
+
+    echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 -y
+    apt-get update && apt-get install -y ansible
+    ansible --version
+else
+    echo " This script works only for $DISTRO "
+fi
